b0e570f427dab9edb7ea220db1bb1caf1be77817
r.arjmandi <r.arjmandi@faraz.com>
2020-02-12 16:21:09 +0330

    task was updated

--- task/sync_wait.hpp
+++ task/sync_wait.hpp
@@ -7,9 +7,8 @@
 
 template<typename AWAITABLE>
 auto sync_wait(AWAITABLE&& awaitable)
-	-> typename awaitable_traits<AWAITABLE&&>::await_result_t
 {
-	auto task = ::detail::make_sync_wait_task(std::forward<AWAITABLE>(awaitable));
+	auto task = make_sync_wait_task(std::forward<AWAITABLE>(awaitable));
 	task.start();
 	return task.result();
 }
